957c8f090532a18b822dc161836fa082e312509a
muntaha <muntahatasnim29@gmail.com>
2019-01-17 20:53:06 +0600

    done

---
+++ app/src/main/java/com/csedu/mediico/my_project/activities/pharmacy_entry.java
@@ -0,0 +1,14 @@
+package com.csedu.mediico.my_project.activities;
+import android.app.Activity;
+import android.os.Bundle;
+import com.csedu.mediico.R;
+
+
+
+public class pharmacy_entry extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pharmacy_entry);
+    }
+}
